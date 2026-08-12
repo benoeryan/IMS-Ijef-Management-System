6d2dc29137b0cca59a70bc2f28d9c96b9c6e485b
ryan benoe <benoeryan21@gmail.com>
2026-08-12 09:47:35 +0700

    fix: finalize report summary header and bump version to 14.9.3

--- core.js
+++ core.js
@@ -287,7 +287,7 @@ async function cleanupFCMToken(userId) {
 }
 
 const ROLES = { admin: 6, bod: 5, head: 4, manager: 3, leader: 2, staff: 1 };
-const APP_VERSION = "14.9.2";
+const APP_VERSION = "14.9.3";
 
 // Indonesian National Holidays 2025
 const HARI_LIBUR_NASIONAL_2025 = [
